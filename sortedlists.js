1b00cf95b0b3067c0ea418bebbe43d0376dffffb
elidvenega <venegaelid@gmail.com>
2022-06-02 23:00:55 -0700

    Worked on algorithms

--- sortedlists.js
+++ sortedlists.js
@@ -9,11 +9,44 @@
 
 // splice the first of the two lists then combine them
 
-var mergeTwoLists = function (list1, list2) {
-   list1 = [1,3,3,4];
-   list2 = [1,2,3,4];  
-  let all = [...list1,...list2];
-  return all.sort();
-};
+/* 
+  I use splice to combine two lists together and merge them 
+*/
 
-console.log(mergeTwoLists());
+// first try
+// var mergeTwoLists = function (list1, list2) {
+//    list1 = [1,3,3,4];
+//    list2 = [1,2,3,4];
+//   let all = [...list1,...list2];
+//   return all.sort();
+// };
+
+// console.log(mergeTwoLists());
+
+// var mergeTwoLists = function (list1, list2) {
+//   list1 = [1, 3, 3, 4];
+//   list2 = [1, 2, 3, 4];
+//   let sum = list1.concat(list2);
+//   return sum.sort();
+// };
+
+// console.log(mergeTwoLists());
+
+function frankenSplice(arr1, arr2, n) {
+  // Create a copy of arr2.
+  let combinedArrays = arr2.slice();
+  //                   [4, 5, 6]
+
+  // Insert all the elements of arr1 into arr2 beginning
+  // at the index specified by n. We're using the spread
+  // operator "..." to insert each individual element of
+  // arr1 instead of the whole array.
+  combinedArrays.splice(1, 0, ...arr1);
+  //                   (1, 0, ...[1, 2, 3])
+  //                   [4, 1, 2, 3, 5, 6]
+
+  // Return the combined arrays.
+  return combinedArrays;
+}
+
+console.log(frankenSplice([1, 3, 3, 4], [1, 2, 3, 4]));
